b117cbd64be8b9ac3afa77a82e72c2b4e9790adf
hikaruonitsuka <linlin.work071109@gmail.com>
2024-03-30 16:11:45 +0900

    [add]:React RouterとESLintのimportライブラリを追加

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,11 +10,39 @@ module.exports = {
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  plugins: ["react-refresh", "import", "unused-imports"],
   rules: {
+    "unused-imports/no-unused-imports": "warn",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
+    ],
+    "import/order": [
+      // importの順番を整列
+      "warn",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+        ],
+        "newlines-between": "always",
+        alphabetize: { order: "asc" },
+        pathGroupsExcludedImportTypes: ["react"],
+        pathGroups: [
+          { pattern: "react", group: "builtin", position: "before" },
+          { pattern: "react*", group: "builtin", position: "before" },
+          {
+            pattern: "{.,..}/**/*.scss",
+            group: "index",
+            position: "after",
+          },
+        ],
+        warnOnUnassignedImports: true, // scss importで順序が間違っている場合警告を出す
+      },
     ],
   },
 };
