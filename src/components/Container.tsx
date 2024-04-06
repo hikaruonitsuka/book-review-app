0641ff51533dd7264fe343066b5ecf8eebc164f8
hikaruonitsuka <linlin.work071109@gmail.com>
2024-04-06 18:24:31 +0900

    [add]:containerにsizeプロパティを追加

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,9 +1,22 @@
+import clsx from 'clsx';
+
 type Props = {
+  size?: 'sm' | 'md' | 'lg';
   children: React.ReactNode;
 };
 
-const Container = ({ children }: Props) => {
-  return <div className="mx-auto w-full max-w-xl px-4">{children}</div>;
+const Container = ({ size = 'md', children }: Props) => {
+  return (
+    <div
+      className={clsx('mx-auto w-full px-4', {
+        'max-w-xl': size === 'sm',
+        'max-w-3xl': size === 'md',
+        'max-w-6xl': size === 'lg',
+      })}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Container;
