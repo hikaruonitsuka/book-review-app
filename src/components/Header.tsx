c5b85e3342de6e267f49645c2c3369b1bd0f0781
hikaruonitsuka <linlin.work071109@gmail.com>
2024-04-22 00:40:02 +0900

    [update]:ヘッダーにユーザー名を表示するように調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,7 @@ import { useAuth } from '@/hooks/useAuth';
 
 const Header = () => {
   const [, , removeCookie] = useCookies(['token']);
-  const { setUser, fetchUser, isLogin } = useAuth();
+  const { user, setUser, fetchUser, isLogin } = useAuth();
   const navigate = useNavigate();
 
   // ユーザー情報の取得
@@ -36,25 +36,28 @@ const Header = () => {
           <Link to="/">BOOK REVIEW APP</Link>
         </h1>
         {isLogin ? (
-          <div className="flex items-center gap-x-4 sm:gap-x-8">
-            <Link to="/new" className="flex items-center gap-x-2">
-              <MessageSquareDiff strokeWidth={2} size={20} />
-              <span className="hidden text-sm font-bold sm:block">
-                レビューを作成する
-              </span>
-            </Link>
-            <Link to="/profile" className="flex items-center gap-x-2">
-              <UserCog strokeWidth={2} size={20} />
-              <span className="hidden text-sm font-bold sm:block">
-                プロフィールを編集
-              </span>
-            </Link>
-            <button className="flex items-center gap-x-2" onClick={logout}>
-              <LogOut strokeWidth={2} size={20} />
-              <span className="hidden text-sm font-bold sm:block">
-                ログアウト
-              </span>
-            </button>
+          <div className="flex items-center gap-x-6 sm:gap-x-10">
+            <div className="font-bold text-orange-600">{user?.name}</div>
+            <div className="flex items-center gap-x-4">
+              <Link to="/new" className="flex items-center gap-x-2">
+                <MessageSquareDiff strokeWidth={2} size={20} />
+                <span className="hidden text-sm font-bold sm:block">
+                  新規レビュー
+                </span>
+              </Link>
+              <Link to="/profile" className="flex items-center gap-x-2">
+                <UserCog strokeWidth={2} size={20} />
+                <span className="hidden text-sm font-bold sm:block">
+                  ユーザー情報
+                </span>
+              </Link>
+              <button className="flex items-center gap-x-2" onClick={logout}>
+                <LogOut strokeWidth={2} size={20} />
+                <span className="hidden text-sm font-bold sm:block">
+                  ログアウト
+                </span>
+              </button>
+            </div>
           </div>
         ) : (
           <div className="flex items-center gap-x-4 sm:gap-x-8">
