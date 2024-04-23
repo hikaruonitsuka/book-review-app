d872a79565a470c78cdb05edecc5c6b2cd6c7664
hikaruonitsuka <linlin.work071109@gmail.com>
2024-04-24 01:37:41 +0900

    [fix]:文字修正

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -6,7 +6,7 @@ const Login = () => {
     <div className="grid min-h-full place-items-center">
       <Container size="sm">
         <section className="flex w-full flex-col gap-y-14">
-          <h2 className="text-center text-2xl font-bold">サインイン</h2>
+          <h2 className="text-center text-2xl font-bold">ログイン</h2>
           <LoginForm />
         </section>
       </Container>
